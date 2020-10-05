bee3a3ebc6d14860059b26c4351edf6c73e61eaa
furiousgodking <603101575@qq.com>
2020-10-05 22:38:46 +0800

    添加菊花状态

--- src/button.vue
+++ src/button.vue
@@ -1,6 +1,7 @@
 <template>
 	<button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
 		<g-icon class="icon" v-if="icon" :name="icon"></g-icon>
+		<g-icon class="loading" name="loading"></g-icon>
 		<div class="content">
 			<slot></slot>
 		</div>
@@ -22,7 +23,11 @@
   }
 </script>
 <style lang="scss">
-  .g-button { font-size: var(--font-size); height: var(--button-height); padding: 0 1em;
+	@keyframes spin {
+		0% { transform: rotate(0deg); }
+		100% { transform: rotate(360deg); }
+	}
+	.g-button { font-size: var(--font-size); height: var(--button-height); padding: 0 1em;
     border-radius: var(--border-radius); border: 1px solid var(--border-color);
     background: var(--button-bg);
     display: inline-flex; justify-content: center; align-items: center;
@@ -36,5 +41,8 @@
       > .content { order: 1; }
       > .icon { order: 2; margin-right: 0; margin-left: .1em;}
     }
+	.loading{
+		animation: spin 2s infinite linear;
+	}
   }
 </style>
