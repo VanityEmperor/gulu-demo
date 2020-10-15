91440d6efabe821ffa5e3f14a4c9c509d90d34d4
furiousgodking <603101575@qq.com>
2020-10-15 10:53:19 +0800

    注释index.html组件

--- src/app.js
+++ src/app.js
@@ -18,12 +18,12 @@ new Vue({
     message: 'hi'
   },
   created(){
-    setTimeout(()=>{
-      let event = new Event('change');
-      let inputElement = this.$el.querySelector('input')
-      inputElement.dispatchEvent(event)
-      console.log('hi')
-    },3000)
+    // setTimeout(()=>{
+    //   let event = new Event('change');
+    //   let inputElement = this.$el.querySelector('input')
+    //   inputElement.dispatchEvent(event)
+    //   console.log('hi')
+    // },3000)
   },
   methods: {
     inputChange (e) {
